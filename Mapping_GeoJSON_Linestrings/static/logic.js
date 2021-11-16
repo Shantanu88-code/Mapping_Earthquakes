2f37a0800a6e948c00048b44e595d015687a06e9
Shanvaid88 <vaidshan88@gmail.com>
2021-11-16 15:36:59 -0500

    Updates JSON Linestrings

---
+++ Mapping_GeoJSON_Linestrings/static/logic.js
@@ -0,0 +1,149 @@
+// Add console.log to check to see if our code is working.
+
+// console.log("working");
+
+// Create the map object with a center at San Fransisco Airport
+
+// let map = L.map('mapid').setView([30, 30], 2);
+
+// Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// // L.geoJSON(geojsonFeature).addTo(map);
+
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// // Grabbing our GeoJSON data
+
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//         console.log(feature);
+//         return L.marker(latlng)
+//         .bindPopup("<h2>" + feature.properties.city + "</h2>");
+//     }
+// }).addTo(map);
+
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer);
+//         layer.bindPopup();
+//     }
+// }).addTo(map);
+
+
+// We're assigning the variable map to the object L.map(), 
+// and we'll instantiate the object with the given string 'mapid'.
+// The mapid will reference the id tag in our <div> element on the index.html file.
+// The setView() method sets the view of the map with a geographical center
+// First coordinate is latitude (40.7) and the second is longitude (-94.5). We set the zoom level of "4" on a scale 0–18
+
+// An alternative to using the setView() method 
+// let map = L.map("mapid", {
+//     center: [
+//         40.7, -94.5
+//     ],
+//     zoom: 4
+// });
+
+// Add a marker to the map
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add marker to the map for Los Angeles with circle
+
+// // Add marker to the map for Los Angeles with circle
+
+// L.circle([34.0522, -118.2437] , {
+//     radius: 100,
+//     color: "black"
+// }).addTo(map);
+
+// L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+// Get data from cities.js
+// let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//  .addTo(map);
+// });
+
+
+// We create the tile layer that will be the background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_key
+});
+
+// Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_key
+});
+
+// Create a base layer that hold maps
+let baseMaps = {
+    Light: light,
+    Dark: dark
+};
+
+// Create a map object with a center and zoom level
+
+let map = L.map("mapid", {
+    center: [44.0, -80.0],
+    zoom: 2,
+    layers: [light]
+});
+
+// Pass or map layers into our layers control and add it to the map.
+L.control.layers(baseMaps).addTo(map); 
+
+// Accesssing airport GeoJSON URL
+
+let torontoData = "https://raw.githubusercontent.com/Shantanu88-code/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Grabbing our GeoJSON Data
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+    // Creating GeoJSON layer with retrieved data.
+    L.geoJson(data, {
+        color: "lightyellow",
+        weight: 2,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+            + feature.properties.dst + "</h3>");
+        }
+    })
+    .addTo(map);
+});
